220307fe1dffd07ceb2cf080881593aa40b90e81
LNUAKX <lukmaannadaf@gmail.com>
2023-12-06 10:54:10 +0530

    added server

--- server/app.js
+++ server/app.js
@@ -1,2 +1,16 @@
-import Users from "./sampledata/Users.js";
-import { checkScore } from "./helpers/findMatch.js";
+import express from "express";
+import ConnectDB from "./database/conn.js";
+
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("hello mates")
+})
+
+
+ConnectDB().then(()=>{
+    app.listen(3000,()=>{
+        console.log("server started at port 3000");
+    })
+})
